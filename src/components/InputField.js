0f2c950dd1da32cc7c0874e6c64d2d27f799206a
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-02-03 22:37:00 +0530

    updated

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,13 +1,22 @@
-import React ,{useRef,useState,useEffect} from 'react';
-import '../styles/App.css';
+import React, { useRef, useState, useEffect } from "react";
+import "../styles/App.css";
 
-//complete the following using forward refs concept 
+//complete the following using forward refs concept
 
-const InputField=()=>{
-    return(
+const InputField = React.forwardRef((prop, ref) => {
+  const ajaj = (event) => {
+    prop.setValue(event.target.value);
+  };
+  return (
     <div>
-      <input id="input" type="text"  ref={ref}/>
+      <input
+        id="input"
+        type="text"
+        ref={ref}
+        value={undefined}
+        onChange={ajaj}
+      />
     </div>
-    )
-    }
+  );
+});
 export default InputField;
